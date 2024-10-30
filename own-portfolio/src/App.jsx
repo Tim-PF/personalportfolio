3038f619121873382857f3b31b1d9acf35dfd322
Tim-PF <tim.pfops@gmail.com>
2024-10-30 20:26:30 +0100

    Grunddesign der Website erstellt

--- own-portfolio/src/App.jsx
+++ own-portfolio/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+import Navbar from "./components/navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  return ( 
+   <div className="d-flex flex-column min-vh-100 w-100">
+    <Navbar></Navbar>
+    <main className="container-fluid flex-grow-1 my-4">
+        <h1>Willkommen auf meiner Website</h1>
+        <p>Hier ist der Hauptinhalt der Seite.</p>
+      </main>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      {/* Footer */}
+      <footer className="bg-light text-center py-3 mt-auto">
+        <p>&copy; 2024 My Website</p>
+      </footer>
+   </div>
+
   )
 }
-
-export default App
+export default App;
